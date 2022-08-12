553ab03584ea1b760bddc5017dca3c4c6a3eac9a
fabien <fabien.vincenty@gmail.com>
2022-08-12 17:07:05 +0200

    helllo app back

---
+++ src/main/java/com/streamermanagersimulator/streamermanagersimulator/StreamermanagersimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.streamermanagersimulator.streamermanagersimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamermanagersimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamermanagersimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamermanagersimulator/streamermanagersimulator/StreamermanagersimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamermanagersimulator.streamermanagersimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamermanagersimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
